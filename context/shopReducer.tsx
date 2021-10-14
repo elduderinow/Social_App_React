1cc852dbdbaefffcdf2190bf0f4f27f8c60b203d
Tom Dirinck <tom.dirinck@gmail.com>
2021-10-14 18:47:58 +0200

    Formatting.

--- context/shopReducer.tsx
+++ context/shopReducer.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state: any, action: any) => {
-        switch (action.type) {
-            case "add": // = action
-                return {...state, cart:[...state.cart,action.payload]}
-            case "delete": // = action
-                let payload = state.cart.filter(elem => elem.id !== action.payload)
-                return {...state, cart:payload}
-            case "updateForm":
-                return {
-                    ...state,
-                    form:{ ...state.form, ...action.payload }
-                }
-
-        }
-    }
-
+  switch (action.type) {
+    case 'add': // = action
+      return { ...state, cart: [...state.cart, action.payload] };
+    case 'delete': // = action
+      let payload = state.cart.filter((elem) => elem.id !== action.payload);
+      return { ...state, cart: payload };
+    case 'updateForm':
+      return {
+        ...state,
+        form: { ...state.form, ...action.payload },
+      };
+  }
+};
