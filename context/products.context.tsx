9ccfb2f69245e66253029b64da13817ea35242aa
Tom Dirinck <tom.dirinck@gmail.com>
2021-10-14 18:47:29 +0200

    Added interface for createContext default value.

--- context/products.context.tsx
+++ context/products.context.tsx
@@ -1,38 +1,55 @@
-import React, {createContext, useReducer} from "react";
+import React, { createContext, useReducer } from 'react';
 import shopReducer from './shopReducer';
 
-
 const products = [
-    {id: 1, name: "iPhone", desc: "iphone 12", price: 400},
-    {id: 2, name: "Laptop", desc: "newest laptop", price: 1200},
-    {id: 3, name: "Samsung", desc: "galaxy s something", price: 300},
-    {id: 4, name: "Midget", desc: "Hobbit with ring", price: 2000},
-    {id: 5, name: "Sweather", desc: "yes its warm", price: 100}
-]
+  { id: 1, name: 'iPhone', desc: 'iphone 12', price: 400 },
+  { id: 2, name: 'Laptop', desc: 'newest laptop', price: 1200 },
+  { id: 3, name: 'Samsung', desc: 'galaxy s something', price: 300 },
+  { id: 4, name: 'Midget', desc: 'Hobbit with ring', price: 2000 },
+  { id: 5, name: 'Sweather', desc: 'yes its warm', price: 100 },
+];
 
-const ShopState = (props:any) => {
-    const initialState:any = {products:products,cart:[], form:{}}
-    const [state, dispatch] = useReducer(shopReducer, initialState)
+const ShopState = (props: any) => {
+  const initialState: any = { products: products, cart: [], form: {} };
+  const [state, dispatch] = useReducer(shopReducer, initialState);
 
-    const addToCart = (product: any) => {
-        dispatch({type: "add", payload: product})
-    }
+  const addToCart = (product: any) => {
+    dispatch({ type: 'add', payload: product });
+  };
 
-    const deleteCartProduct = (id:number) => {
+  const deleteCartProduct = (id: number) => {
+    dispatch({ type: 'delete', payload: id });
+  };
 
-        dispatch({type: "delete", payload: id})
-    }
+  const updateForm = (data: any) => {
+    dispatch({ type: 'updateForm', payload: data });
+  };
 
-    const updateForm = (data:any) => {
-        dispatch({type: "updateForm", payload: data})
-    }
+  return (
+    <ProductsContext.Provider
+      value={{
+        products: state.products,
+        cart: state.cart,
+        form: state.form,
+        addToCart,
+        deleteCartProduct,
+        updateForm,
+      }}
+    >
+      {props.children}
+    </ProductsContext.Provider>
+  );
+};
 
-    return <ProductsContext.Provider value={{products:state.products,cart:state.cart, form:state.form, addToCart, deleteCartProduct, updateForm}}>
-            {props.children}
-            </ProductsContext.Provider>
+export default ShopState;
+
+interface Provider {
+  products: [];
+  cart: [];
+  form: {};
+  addToCart: (product: any) => void;
+  deleteCartProduct: (id: number) => void;
+  updateForm: (data: any) => void;
 }
 
-export default ShopState
-
-export const ProductsContext = createContext(null);
-
+export const ProductsContext = createContext({} as Provider);
